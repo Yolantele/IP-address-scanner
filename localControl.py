692d0e2c149d2c27f1e6faa6ca5cc1c5257881c6
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-08-31 11:30:57 +0100

    Tests running terminal commands via running script

---
+++ localControl.py
@@ -0,0 +1,9 @@
+import os
+from subprocess import call
+
+call(["ls", "-l"])
+
+# if you want to pass some variable in the script this gives very easy way for example take the following part of the code
+
+# abc = a.c
+# call(["vim", abc])
