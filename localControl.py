9647cb2af3b7269050a444241449d2aae836b797
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-08-31 12:00:46 +0100

    Tests running network scan via script

--- localControl.py
+++ localControl.py
@@ -1,13 +1,13 @@
 import os
 from subprocess import call
 
-run_root = "sudo su"
-activate_pyenv = "source trackerjacker_env"
+# run_root = "sudo su"
+# activate_pyenv = "source trackerjacker_env/bin/activate"
 run_trackerjacker = "trackerjacker -i wlan1 --map --map-file ./maps/wifi_map_31Aug_12pm_JigsawBuilding.yaml"
 
 
 # call(["ls", "-l"])
-call([run_root, activate_pyenv, run_trackerjacker])
+call([run_trackerjacker])
 
 
 # if you want to pass some variable in the script:
