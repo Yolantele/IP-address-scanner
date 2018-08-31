2f80a41e939ea7114338047bb6330f30504fe2a6
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-08-31 13:08:43 +0100

    removes activating of python environment as this need to be run seperately

--- localControl.py
+++ localControl.py
@@ -3,8 +3,9 @@ from subprocess import call
 import datetime
 
 # build map name:
-date= str(datetime.datetime.now().strftime("%d%b%y-%I%p"))
-building= '_JigsawBuilding'
+in_format = "%d%b%y-%I%p"
+date = str(datetime.datetime.now().strftime(in_format))
+building = '_JigsawBuilding'
 file_type = '.yaml'
 
 # configure trackerjacker:
@@ -18,13 +19,13 @@ interface_name = 'wlan1'
 provide_interface = ' --interface ' + interface_name
 
 
-run_root = "sudo su"
-activate_pyenv = "source trackerjacker_env/bin/activate"
+# run_root = "sudo su"
+# activate_pyenv = "source trackerjacker_env/bin/activate"
 run_trackerjacker = 'trackerjacker --map ' + provide_map + provide_interface
 
 
 # calls commands inside shell's location (can provide directory):
-call([run_root, activate_pyenv, run_trackerjacker], shell=True)
+call([activate_pyenv, run_trackerjacker], shell=True)
 
 
 # if you want to pass some variable in the script:
