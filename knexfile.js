4b06517dcc6c73b7e7aaabe9033f01d9cfcbecfb
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-09-03 15:42:55 +0100

    Sets DB name

--- knexfile.js
+++ knexfile.js
@@ -5,47 +5,23 @@ require('dotenv').config();
 const path = require("path");
 
 const BASE_PATH = path.join(__dirname, "db");
-const DATABASE = process.env.NETWORK_SCANNER_DB_NAME;
-console.log(NETWORK_SCANNER_DB_NAME)
-console.log(DATABASE)
+const DATABASE =  "network_scanner" // or process.env.NETWORK_SCANNER_DB_NAME;
 
-console.log("In migrations file");
+console.log("-------------> In migrations file");
 console.log(BASE_PATH);
 
 module.exports = {
   test: {
     client: "pg",
-    connection: `
-postgres: //${process.env.LOCAL_DB_USERNAME}:${
-    process.env.LOCAL_DB_PASSWORD
-}
-@localhost: 5432 / $ {
-    DATABASE
-}
-_test `,
-    migrations: {
-      directory: path.join(BASE_PATH, "migrations", "V0.5.0")
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, "seeds/test")
-    }
+    connection: `postgres://${process.env.LOCAL_DB_USERNAME}:${process.env.LOCAL_DB_PASSWORD}@localhost:5432/${DATABASE}_test `,
+    migrations: {directory: path.join(BASE_PATH, "migrations", "V0.5.0")},
+    seeds: {directory: path.join(BASE_PATH, "seeds/test")}
   },
   development: {
     client: "pg",
-    connection: `
-postgres: //${process.env.LOCAL_DB_USERNAME}:${
-    process.env.LOCAL_DB_PASSWORD
-}
-@localhost: 5432 / $ {
-    DATABASE
-}
-`,
-    migrations: {
-      directory: path.join(BASE_PATH, "migrations", "V0.5.0")
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, "seeds/dev")
-    }
+    connection: `postgres://${process.env.LOCAL_DB_USERNAME}:${process.env.LOCAL_DB_PASSWORD}@localhost:5432/${DATABASE}`,
+    migrations: {directory: path.join(BASE_PATH, "migrations", "V0.5.0")},
+    seeds: {directory: path.join(BASE_PATH, "seeds/dev")}
   },
   production: {
     client: "pg",
