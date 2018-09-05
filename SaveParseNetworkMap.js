1c90248a106d1312c3f04422b48e8d97139b1898
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-09-05 09:03:55 +0100

    Moves crone and yaml tasks to python packet

--- SaveParseNetworkMap.js
+++ SaveParseNetworkMap.js
@@ -1,26 +1,9 @@
-const yaml = require('yamljs');
-const fs = require('fs');
-const cron = require('node-cron');
 const networkServices = require('./services/networkServices')
 
-// const laptopLocation = `/Users/jolanta/Projects/2018/network-scanner/maps/`;
-// const rPiLocation = `/home/pi/trackerjacker/maps/`;
-
-// const PATH = laptopLocation
 const BUILDING = "Morelands, Jigsaw XYZ"
 
-// converts YAML to stringified JSON:
-const yamlToJson = mapName => {
-  try {
-    return yaml.parse(mapName.data);
-  } catch (e) {
-    console.log(e)
-  }
-}
 
-//parses stgringified JSON into sanitized JSON obj:
 const parseNetworkData = mapName => {
-  console.log('----------> map comes in ')
   let devicesAccrossNetworks = 0
 
   Object.entries(mapName).forEach(([network, networkValue]) => {
@@ -33,7 +16,6 @@ const parseNetworkData = mapName => {
     if (devices === 0) {
       null
     } else {
-      
       devicesAccrossNetworks += devices
       networkServices.addNetworkDevicesToDB(network, devicesAccrossNetworks, BUILDING, 
         (err, networkData) => {
@@ -42,26 +24,10 @@ const parseNetworkData = mapName => {
           }
         return callback(null, networkData);
       })
-
     }
-    console.log('----------> successfully updated row with network devices data')
   })
 }
 
-// const readForParsing = fs.readdirSync(PATH).map(file => {
-//   parseNetworkData(String(file));
-// })
-
-
-// // periodically runs these commands:
-// cron.schedule('* * * * *', () => { // saves every minute
-//   readForParsing
-//   console.log(`-----------> PARSED THE MAP `)
-// });
-
-
-
 module.exports = {
-  yamlToJson,
   parseNetworkData,
 }
