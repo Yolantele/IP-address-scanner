4335c69fdc8f3bff96237535c51de7eb52e82d61
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-09-13 15:39:50 +0100

    Return id field for the wifi speed data

--- services/networkServices.js
+++ services/networkServices.js
@@ -55,7 +55,7 @@ module.exports = {
   listAllWifiData (callback) {
     dbServices
       .knex(WIFI_TABLE)
-      .select('wifi_ip', 'wifi_speed_json', 'building', 'created_at' )
+      .select('id', 'wifi_ip', 'wifi_speed_json', 'building', 'created_at' )
       .orderBy('created_at', 'desc')
       .then(instances => {
         return callback(null, instances);
