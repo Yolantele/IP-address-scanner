235cc13f08ed7e64594662a3dd4ea4608b628ed1
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-10-01 11:39:24 +0100

    Adds middleware to require an accessToken for accessing of all the data APIs

--- app-setup.js
+++ app-setup.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cheeseToastie = require("cheese-toastie");
 const fileUpload = require("express-fileupload");
 const middlewares = require('./middlewares');
-
+const tokens = require('./tokens')
 var cors = require("cors");
 
 // const loggerServices = require("./services/logger-services");
@@ -18,6 +18,14 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(middlewares.tracking());
 app.use(middlewares.requestInit());
+
+// provide correct x-user-token to access all api endpoints:
+cheeseToastie.setAuthenticatorMethod((token, callback) => {
+    if (token && token == tokens.accessToken) {
+        return callback({authorized: "correct token provided, access to data is authorized"});
+    }
+    return callback(null)
+})
 
 cheeseToastie.start(__dirname, app);
 
